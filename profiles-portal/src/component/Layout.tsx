2bcfca50a78e3458c0c50be24c2b1d9f0e1d5758
Kanika <kanikaverma0510@gmail.com>
2025-07-15 14:01:24 +0530

    wallet and blockchain connect

--- profiles-portal/src/component/Layout.tsx
+++ profiles-portal/src/component/Layout.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { ReactNode } from 'react'
 import Link from 'next/link'
+import { ConnectButton } from '@suiet/wallet-kit'
 
 interface LayoutProps {
   children: ReactNode
@@ -8,16 +11,23 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-white font-serif flex flex-col">
-      <header className="bg-gradient-to-b from-blue-200 to-blue-800 text-white flex items-center">
-        <Link href="/" className="flex items-center text-5xl font-bold">
+      <header className="bg-gradient-to-b from-blue-200 to-blue-800 text-white flex items-center justify-between px-6 py-4">
+       
+        <Link href="/" className="flex items-center text-3xl font-bold">
           <img
             src="/logo.png"
             alt="Logo"
-            className="h-30 w-30 mr-2 object-contain"
+            className="h-12 w-12 mr-2 object-contain"
           />
           Make Your Profile
         </Link>
+
+      
+        <div>
+          <ConnectButton />
+        </div>
       </header>
+
       <main className="flex-1 p-8">{children}</main>
     </div>
   )
